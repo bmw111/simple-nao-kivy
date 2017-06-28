87a03a29aea3263bba6d732886a3ce8c5ddfa487
zyqzyq <497533265@qq.com>
2017-06-28 09:03:08 +0800

    add module sqlites3 trying package

--- buildozer.spec
+++ buildozer.spec
@@ -36,7 +36,7 @@ version = 1.0
 
 # (list) Application requirements
 # comma seperated e.g. requirements = sqlite3,kivy
-requirements = kivy,python2
+requirements = kivy,python2,sqlite3
 
 # (str) Custom source folders for requirements
 # Sets custom source for any requirements with recipes
@@ -46,10 +46,10 @@ requirements = kivy,python2
 #garden_requirements =
 
 # (str) Presplash of the application
-#presplash.filename = %(source.dir)s/data/presplash.png
+presplash.filename = %(source.dir)s/data//images/presplash.png
 
 # (str) Icon of the application
-#icon.filename = %(source.dir)s/data/icon.png
+icon.filename = %(source.dir)s/data/images/nao.png
 
 # (str) Supported orientation (one of landscape, portrait or all)
 orientation = landscape
--- main.py
+++ main.py
@@ -2,46 +2,139 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.text import LabelBase 
+from kivy.core.window import WindowBase
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
-from kivy.properties import NumericProperty,ReferenceListProperty,BooleanProperty
+from kivy.uix.screenmanager import ScreenManager, Screen,TransitionBase,FadeTransition
+from kivy.properties import NumericProperty,ReferenceListProperty,BooleanProperty,ObjectProperty,ListProperty
 from kivy.graphics import Color,Line,Rectangle
 from kivy.vector import Vector
 import socket
 from kivy.core.image import Image as CoreImage
+import threading
+import sqlite3
 
-class MyWidget(BoxLayout):
+class MyScreenManager(ScreenManager):
+	def __init__(self,*args, **kwargs):
+		super(MyScreenManager,self).__init__(*args, **kwargs)
+		self.transition = FadeTransition()
+		self.add_widget(MenuScreen())
+		self.add_widget(MoveScreen())
+		self.add_widget(SayScreen())
+		self.add_widget(DanceScreen())
+		self.add_widget(EmailScreen())
+		self.add_widget(EditScreen())
+
+
+	def updata_say_screen(self):
+		if self.has_screen('say'):
+			self.remove_widget(self.get_screen('say'))
+		self.add_widget(SayScreen())
+		self.current = 'say'
+
+class MenuScreen(Screen):
+	pass
+class MoveScreen(Screen):
+	pass
+class SayScreen(Screen):
+	say_text_list = ListProperty([])
+	def __init__(self,*args, **kwargs):
+		super(SayScreen,self).__init__(*args, **kwargs)
+		self.updata_say_text()
+		self.get_layout()
+	def get_layout(self):
+		layout = BoxLayout(orientation='vertical',pos_hint={'x':.2,'top':.6},size_hint=(.6,.4))
+		layout_2 = BoxLayout()
+		textinput = TextInput(text='请在此输入想说的话')
+		button_say = Button(text='say',background_color=[0,0,1,1])
+		button_say.bind(on_press= lambda x:cli.say(textinput.text.encode('utf-8')))
+		layout_2.add_widget(textinput)
+		layout_2.add_widget(button_say)
+		layout.add_widget(layout_2)
+		#print self.direct.say_text_list
+		
+		text1 = self.say_text_list[0][1].encode('utf-8')
+		layout.add_widget(Button(background_color=[0,0,1,1],text=text1,on_press=lambda x:cli.say(text1)))
+		text2 = self.say_text_list[1][1].encode('utf-8')
+		layout.add_widget(Button(background_color=[0,0,1,1],text=text2,on_press=lambda x:cli.say(text2)))
+		text3 = self.say_text_list[2][1].encode('utf-8')
+		layout.add_widget(Button(background_color=[0,0,1,1],text=text3,on_press=lambda x:cli.say(text3)))
+		text4 = self.say_text_list[3][1].encode('utf-8')
+		layout.add_widget(Button(background_color=[0,0,1,1],text=text4,on_press=lambda x:cli.say(text4)))
+
+		self.add_widget(layout)
+	
+	def updata_say_text(self):
+		self.text_list = []
+		conn = sqlite3.connect('text.db')
+		cu = conn.cursor()
+		get_text_sql = '''SELECT * FROM tts_text'''
+		cu.execute(get_text_sql)
+		text_values = cu.fetchall()
+		for text in text_values:
+			self.text_list.append(text)
+		#print say_text_list
+		self.say_text_list = self.text_list
+		conn.commit()
+		cu.close()
+
+class DanceScreen(Screen):
+    pass
+	
+class EditScreen(Screen):
+	#sm = MyScreenManager()
+	def __init__(self,*args, **kwargs):
+		super(EditScreen,self).__init__(*args, **kwargs)
+		pass
+		#MyScreenManager().updata_say_screen()
+	def updata_say_text_list(self,text_list):
+		data = []
+		conn = sqlite3.connect('text.db')
+		cu = conn.cursor()
+		update_sql = 'UPDATE tts_text SET text = ? WHERE id = ? '
+		for i in range(4):
+			data.append((text_list[i],i+1))
+			cu.execute(update_sql,data[i])
+		conn.commit()
+		cu.close()
+		#self.sm.updata_say_screen()
+		#updata_text_sql
+		
+class EmailScreen(Screen):
+	pass
+
+class MyUdpClient(threading.Thread):
 	address = ('<broadcast>', 11000)  
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  
-	def __init__(self, **kwargs):
-		super(MyWidget, self).__init__(**kwargs)
-		#self.say()
-		#print '123'
+	def __init__(self,*args, **kwargs):
+		super(MyUdpClient,self).__init__(*args, **kwargs)
 		pass
 	def sengmsg(self,operation,text):
 		msg = operation+text
-		#print msg
+		print msg
 		self.s.sendto(msg,self.address)
 	def stop(self):
 		#print 'stop'
 		self.sengmsg('00',' ')
 	def move(self,dir,angle):
 		self.sengmsg('01',dir+str(angle))
-	def say(self):
+	def say(self,text):
 		#print 'say'
-		self.sengmsg('02','你好，欢迎参观赫瓦机器人')
-	def dance(self):
+		self.sengmsg('02',text)
+	def dance(self,text):
 		#print 'dance'
-		self.sengmsg('03','小苹果')
+		self.sengmsg('03',text)
 	def voice(self,text):
 		self.sengmsg('04',text)
 	
 		
 class MyDirectionlever(Widget):
-	widget = MyWidget()
+	
 	padding = NumericProperty(60) #方向距离侧边的距离
 	ball_x = NumericProperty(0)  
 	ball_y = NumericProperty(0)
@@ -88,7 +181,7 @@ class MyDirectionlever(Widget):
 		self.resize(self,self.size)
 		if self.is_moving:
 			self.is_moving=False
-			self.widget.stop()
+			cli.stop()
 	def on_touch_move(self,args):
 		if self.is_moving:
 			self.move_time +=1
@@ -106,10 +199,10 @@ class MyDirectionlever(Widget):
 					#print angle
 					if angle>0:
 						angle = angle-90
-						self.widget.move('00',angle/90)
+						cli.move('00',angle/90)
 					else:
 						angle = -90-angle
-						self.widget.move('01',angle/-90)
+						cli.move('01',angle/-90)
 	def updata_ball(self):
 		self.canvas.after.clear()
 		with self.canvas.after:
@@ -119,9 +212,44 @@ class IconButton(ButtonBehavior, Image):
     pass
 
 class DirectionleverApp(App):
+	import os,sys
+	
+	#os.chdir(sys.path[0])
+	cli=MyUdpClient()
+	say_text_list = ListProperty([])
+	text_list = []
+	conn = sqlite3.connect('text.db')
+	cu = conn.cursor()
+	get_text_sql = '''SELECT * FROM tts_text'''
+	cu.execute(get_text_sql)
+	text_values = cu.fetchall()
+	for text in text_values:
+		text_list.append(text)
+	say_text_list = text_list
+	#print say_text_list
+	conn.commit()
+	cu.close()
 	def build(self):
 		self.icon = 'data/images/nao.png'
+		WindowBase.softinput_mode='below_target'
 		LabelBase.register('Roboto','data/ttf/DroidSansFallback.ttf')
-	
+		return MyScreenManager()
+	def updata_say_text(self):
+		self.text_list = []
+		conn = sqlite3.connect('text.db')
+		cu = conn.cursor()
+		get_text_sql = '''SELECT * FROM tts_text'''
+		cu.execute(get_text_sql)
+		text_values = cu.fetchall()
+		for text in text_values:
+			self.text_list.append(text)
+		#print say_text_list
+		self.say_text_list = self.text_list
+		conn.commit()
+		cu.close()
+		print '123'
+		print self.say_text_list
 if __name__=='__main__':
-    DirectionleverApp().run()
+	
+	cli=MyUdpClient()
+	DirectionleverApp().run()
